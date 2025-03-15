fix bug in position calculation

humanoid/envs/base/legged_robot.py
@@ -100,7 +100,6 @@ class LeggedRobot(BaseTask):
                 self.gym.fetch_results(self.sim, True)
             self.gym.refresh_dof_state_tensor(self.sim)
         self.post_physics_step()
-
         # return clipped obs, clipped states (None), rewards, dones and infos
         clip_obs = self.cfg.normalization.clip_observations
         self.obs_buf = torch.clip(self.obs_buf, -clip_obs, clip_obs)
@@ -116,8 +115,8 @@ class LeggedRobot(BaseTask):
         self.reset_idx(torch.arange(self.num_envs, device=self.device))
         obs, privileged_obs, _, _, _ = self.step(torch.zeros(
             self.num_envs, self.num_actions, device=self.device, requires_grad=False))
+        self.init_rigid_states = self.rigid_state.clone()
         # reset initial_rigid_staete
-        self.initial_rigid_states = self.rigid_state.clone()
         return obs, privileged_obs
     
     def post_physics_step(self):
@@ -145,7 +144,6 @@ class LeggedRobot(BaseTask):
         self.compute_reward()
         env_ids = self.reset_buf.nonzero(as_tuple=False).flatten()
         self.reset_idx(env_ids)
-
         self.compute_observations() # in some cases a simulation step might be required to refresh some obs (for example body positions)
 
         self.last_last_actions[:] = torch.clone(self.last_actions[:])
@@ -158,8 +156,7 @@ class LeggedRobot(BaseTask):
         if self.viewer and self.enable_viewer_sync and self.debug_viz:
             self._draw_debug_vis()
 
-        # reset initial_rigid_staete
-        self.initial_rigid_states[env_ids, :, :] = self.rigid_state[env_ids, :, :].clone()
+
 
 
     def check_termination(self):
@@ -223,6 +220,8 @@ class LeggedRobot(BaseTask):
         self.base_quat[env_ids] = self.root_states[env_ids, 3:7]
         self.base_euler_xyz = get_euler_xyz_tensor(self.base_quat)
         self.projected_gravity[env_ids] = quat_rotate_inverse(self.base_quat[env_ids], self.gravity_vec[env_ids])
+
+
 
 
 
@@ -523,7 +522,7 @@ class LeggedRobot(BaseTask):
 
         self.contact_forces = gymtorch.wrap_tensor(net_contact_forces).view(self.num_envs, -1, 3) # shape: num_envs, num_bodies, xyz axis
         self.rigid_state = gymtorch.wrap_tensor(rigid_body_state).view(self.num_envs, -1, 13)
-        self.initial_rigid_states = torch.zeros(self.num_envs, 30, 13, dtype=torch.float, device=self.device, requires_grad=False)
+        self.init_rigid_states = torch.zeros(self.num_envs, 30, 13, dtype=torch.float, device=self.device, requires_grad=False)
         self.torso_idx = self.gym.find_actor_rigid_body_handle(self.envs[0], self.actor_handles[0], "torso")
         self.torso_state = self.rigid_state[:, self.torso_idx]
         # initialize some data used later on